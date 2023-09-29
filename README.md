a186c99e24c38ec196f87ff42bcaa0fe43731a34
RetroEdit <retroeditcode@gmail.com>
2023-09-29 16:53:52 +0000

    save_edit: add CLI save editor

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # mr_platformer
 Tools and info for Terry Cavanagh's 2023 game Mr. Platformer
+
+## Save editor
+Mr. Platformer has four different level types: forest, volcano, factory, and beach. The level types are randomly arranged on the map, but the level progression itself is not random. Each level type has a sequential progression of 10 primary layouts followed by one final layout.
+
+The save editor provides some basic convenience arguments to modify the current level index for each type.
+
+Requirements:
+- Python 3.6 or later: if you don't have Python, you can look into installing it from here: https://www.python.org/downloads/
+- Windows: the code is currently Windows-specific, but it could be easily modified to work on other platforms by adding alternate save paths.
+
+### Usage examples
+The save editor is currently a command-line program.
+
+You'll want to open up your shell of choice and navigate to the directory where the script is downloaded. On Windows, the basic installed shells are Command Prompt and Powershell.
+
+Display help:
+```
+python mr_platformer_saves.py -h
+```
+
+End any game processes, including those running in the background:
+```
+python mr_platformer_saves.py --closegame
+```
+
+End game processes and display the save file's contents:
+```
+python mr_platformer_saves.py --closegame --action read
+```
+
+End game processes and delete the save file:
+```
+python mr_platformer_saves.py --closegame --action reset
+```
+
+End game processes and overwrite the save file with one that starts with one forest level completed:
+```
+python mr_platformer_saves.py --closegame --action overwrite --forest 2
+```
+
+End game processes and overwrite the save file with one that starts at the first forest level with all other level types completed:
+```
+python mr_platformer_saves.py --closegame --action overwrite --forest 1 --autofill 11
+```
---
+++ mr_platformer_saves.py
@@ -0,0 +1,88 @@
+
+import argparse
+from collections import namedtuple, OrderedDict
+import os
+import struct
+
+GameSave = namedtuple('GameSave',
+    ('forest_level', 'volcano_level', 'factory_level', 'beach_level',
+    'unknown0', 'unknown1', 'unknown2', 'unknown3',
+    'all_status', 'beach_status', 'factory_status', 'forest_status', 'volcano_status', 'high_score'))
+
+DEFAULT_SAVE = GameSave(1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,)
+GAME_SAVE_FIELD_COUNT = 14
+SAVE_FIELD_PREFIX = (8, 2)
+# Could be an Enum
+LEVEL_TYPES = ('forest', 'volcano', 'factory', 'beach')
+SAVE_ACTIONS = ('read', 'reset', 'overwrite')
+
+def get_save_path():
+    # Windows-specific
+    return os.path.join(os.getenv('APPDATA'),
+        'Godot', 'app_userdata', 'Mr. Platformer', 'progress.save')
+
+def end_game_process():
+    # Windows-specific
+    print("[ending game processes]")
+    os.system('''wmic process where "name='mrplatformer.exe'" delete''')
+
+def read_save(save_path, message="read save"):
+    save_items = []
+    with open(save_path, 'rb') as infile:
+        for i in range(GAME_SAVE_FIELD_COUNT):
+            field_triplet = struct.unpack('<3i', infile.read(3*4))
+            assert field_triplet[:2] == SAVE_FIELD_PREFIX
+            save_items.append(field_triplet[2])
+    print(f"[{message}]\ncontents: {GameSave(*save_items)}")
+
+def overwrite_save(save_path, save):
+    read_save(save_path, "previous save contents")
+    save_items = GameSave(*save.values())
+    print(f"[overwriting save]\ncontents: {save_items}")
+    with open(save_path, 'wb') as outfile:
+        for item in save_items:
+            outfile.write(struct.pack('<3i', *SAVE_FIELD_PREFIX, item))
+
+def delete_save(save_path):
+    read_save(save_path, "previous save contents")
+    print("[deleting save]")
+    try:
+        os.remove(save_path)
+    except FileNotFoundError:
+        pass
+
+def main():
+    parser = argparse.ArgumentParser(prog='mr_platformer_saves.py',
+        description="Save editing tools for Terry Cavanagh's 2023 game Mr. Platformer",)
+    parser.add_argument('--action', help='action to apply to save file', choices=SAVE_ACTIONS)
+    for level in LEVEL_TYPES:
+        parser.add_argument(f'--{level}', type=int, help=f'{level} levels index')
+    parser.add_argument('--autofill', type=int, help='autofill non-specified level indexes with this value')
+    parser.add_argument('--closegame', help='close any remaining game processes, foreground or background', action='store_true')
+    if os.name != 'nt':
+        # get_save_path and end_game_process are both Windows-specific
+        raise OSError('Only Windows is currently supported')
+    args = parser.parse_args()
+    save_path = get_save_path()
+    if args.closegame:
+        end_game_process()
+    action = args.action
+    if action == 'overwrite':
+        save = OrderedDict(DEFAULT_SAVE._asdict())
+        autofill_value = args.autofill
+        for level in LEVEL_TYPES:
+            level_index = getattr(args, level)
+            if level_index is not None:
+                save[f'{level}_level'] = level_index
+            elif autofill_value is not None:
+                save[f'{level}_level'] = autofill_value
+        overwrite_save(save_path, save)
+    elif action == 'reset':
+        delete_save(save_path)
+    elif action == 'read':
+        read_save(save_path)
+    elif not args.closegame:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
